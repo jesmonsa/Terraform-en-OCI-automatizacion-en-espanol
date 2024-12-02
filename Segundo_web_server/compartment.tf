5b01ef63252cc2e9fae45ec96c3ccca376ecab6d
Jesus Montoya <jesmonsa2018@gmail.com>
2024-12-02 05:45:52 -0500

    Modificacion provider

---
+++ Segundo_web_server/compartment.tf
@@ -0,0 +1,11 @@
+resource "oci_identity_compartment" "produccionCompartment" {
+  provider = oci.homeregion
+  name = "produccionCompartment"
+  description = "produccion Compartment"
+  compartment_id = var.compartment_ocid
+  
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
+}
+
